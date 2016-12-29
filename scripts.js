00f46ef005bc607dbe7168491c68f9b9595286ff
AyushGR <ayush.ramkumar@gmail.com>
2016-12-29 11:37:43 -0500

    new

--- scripts.js
+++ scripts.js
@@ -1,10 +1,35 @@
-var timesarray;
+var STORAGE_KEY = 'data';
 
-var storeInput() {
-	document.getElementById('enter').
+function setObject(key, value) {
+	localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getObject(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+
+var weightData = [];
+
+if (localStorage.getObject(STORAGE_KEY) != undefined) {
+	weightData = getObject(STORAGE_KEY);
+}
+
+window.onbeforeunload = function() {
+	console.log('close attempted');
+	return "are you sure?";
+}
+
+function storeData() {
+	var weight = document.getElementById('weight').value;
+	var date = document.getElementById('date').value;
+
+	if (!isNaN(weight)) {
+		weightData.push({date: date, weight: weight});
+	}
+
+	localStorage.setObject('data', weightData);
 } 
 
-var displayOutput() {
 
-}
 
